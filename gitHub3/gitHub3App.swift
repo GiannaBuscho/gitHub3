47d9949ea0d78d4253e6ec1d1c39018e79f425ee
Gianna Buscho <giannabuscho@gmail.com>
2023-06-12 08:24:46 -0700

    Initial Commit

---
+++ gitHub3/gitHub3App.swift
@@ -0,0 +1,17 @@
+//
+//  gitHub3App.swift
+//  gitHub3
+//
+//  Created by scholar on 6/12/23.
+//
+
+import SwiftUI
+
+@main
+struct gitHub3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
